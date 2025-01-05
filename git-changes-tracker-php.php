380bf887f0ba5ec7daa32e681b13fe714b829ec0
pradnyeshbhoge123 <pradnyeshbhoge123@gmail.com>
2025-01-05 20:31:16 +0530

    Fix whitespace in timezone setting for consistency in `git-changes-tracker-php.php`

--- git-changes-tracker-php.php
+++ git-changes-tracker-php.php
@@ -1,5 +1,5 @@
 <?php
-    date_default_timezone_set('Asia/Kolkata'); // Ensure consistent timezone
+    date_default_timezone_set('Asia/Kolkata'); 
 
 class GitChangesTracker {
     private $repoPath;
